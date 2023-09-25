fd5d0c3c0ce9d20ff3c0d0fafbc055031f871968
pdmuds4 <s2222015@stu.musashino-u.ac.jp>
2023-09-25 23:00:12 +0900

    update test_demucs

--- test_demucs.py
+++ test_demucs.py
@@ -1,1 +1,8 @@
-#test
+import demucs.separate
+
+options = ["sample/no_vocal.wav",
+           "-n", "htdemucs",
+           #"--two-stems", "vocals",
+           "--mp3"]
+
+demucs.separate.main(options)
